1694383e9cafa82b006aa3f4c1c80746fead0851
gun0912 <gun0912@selphone.co.kr>
2016-07-06 18:47:04 +0900

    - fix #16 - add BuildConfig.DEBUG option

--- tedpermission/src/main/java/com/gun0912/tedpermission/util/Dlog.java
+++ tedpermission/src/main/java/com/gun0912/tedpermission/util/Dlog.java
@@ -1,49 +1,53 @@
 package com.gun0912.tedpermission.util;
 
 import android.util.Log;
-
+import com.gun0912.tedpermission.BuildConfig;
 
 public class Dlog {
 
-	static final String TAG = "tedpark";
+  static final String TAG = "tedpark";
 
-	
-	 /** Log Level Error **/
-	public static  void e(String message) {
-		Log.e(TAG, buildLogMsg(message));
-	}
-	 /** Log Level Warning **/
-	public static  void w(String message) {
-		Log.w(TAG, buildLogMsg(message));
-	}
-	 /** Log Level Information **/
-	public static  void i(String message) {
-		Log.i(TAG, buildLogMsg(message));
-	}
-	/** Log Level Debug **/
-	public static  void d(String message) {
-		Log.d(TAG, buildLogMsg(message));
-	}
-	/** Log Level Verbose **/
-	public static  void v(String message){Log.v(TAG, buildLogMsg(message));
-	}
-	
+  /** Log Level Error **/
+  public static void e(String message) {
 
-	public static String buildLogMsg(String message) {
+    if (BuildConfig.DEBUG) Log.e(TAG, buildLogMsg(message));
+  }
 
-		StackTraceElement ste = Thread.currentThread().getStackTrace()[4];
+  /** Log Level Warning **/
+  public static void w(String message) {
+    if (BuildConfig.DEBUG) Log.w(TAG, buildLogMsg(message));
+  }
 
-		StringBuilder sb = new StringBuilder();
+  /** Log Level Information **/
+  public static void i(String message) {
 
-		sb.append("[");
-		sb.append(ste.getFileName().replace(".java", ""));
-		sb.append("::");
-		sb.append(ste.getMethodName());
-		sb.append("]");
-		sb.append(message);
+    if (BuildConfig.DEBUG) Log.i(TAG, buildLogMsg(message));
+  }
 
-		return sb.toString();
+  /** Log Level Debug **/
+  public static void d(String message) {
 
-	}
+    if (BuildConfig.DEBUG) Log.d(TAG, buildLogMsg(message));
+  }
 
+  /** Log Level Verbose **/
+  public static void v(String message) {
+    if (BuildConfig.DEBUG) Log.v(TAG, buildLogMsg(message));
+  }
+
+  public static String buildLogMsg(String message) {
+
+    StackTraceElement ste = Thread.currentThread().getStackTrace()[4];
+
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("[");
+    sb.append(ste.getFileName().replace(".java", ""));
+    sb.append("::");
+    sb.append(ste.getMethodName());
+    sb.append("]");
+    sb.append(message);
+
+    return sb.toString();
+  }
 }
