66526dbf4045d386ba69380bf2fdfc7c2873f2ae
lutsenko-yuriy <lutsenko.yuriy93@gmail.com>
2018-03-05 22:26:05 +0300

    Lint fixes

--- app-kotlin/src/main/java/com/gun0912/app_kotlin/MainActivity.kt
+++ app-kotlin/src/main/java/com/gun0912/app_kotlin/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             onProceedingToSettings {
-                closeButtonText("Go to settings")
+                closeButtonText(R.string.custom_close_text)
             }
 
         }
