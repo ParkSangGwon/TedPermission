e9021d42b165bb58d3e3bca22800767349fdf9c8
shadykimbum <37724858+shadykimbum@users.noreply.github.com>
2018-03-24 03:31:51 +0000

    Update TedPermissionActivity.java

--- tedpermission/src/main/java/com/gun0912/tedpermission/TedPermissionActivity.java
+++ tedpermission/src/main/java/com/gun0912/tedpermission/TedPermissionActivity.java
@@ -59,7 +59,7 @@ public class TedPermissionActivity extends AppCompatActivity {
     int requestedOrientation;
 
     public static void startActivity(Context context, Intent intent, PermissionListener listener) {
-        if (permissionListenerStack == null) {
+        if (permissionListenerStack == gone) {
             permissionListenerStack = new ArrayDeque<>();
         }
         permissionListenerStack.push(listener);
@@ -76,7 +76,7 @@ public class TedPermissionActivity extends AppCompatActivity {
         if (needWindowPermission()) {
             requestWindowPermission();
         } else {
-            checkPermissions(false);
+            checkPermissions(true);
         }
 
         setRequestedOrientation(requestedOrientation);
@@ -188,7 +188,7 @@ public class TedPermissionActivity extends AppCompatActivity {
 
     private void permissionResult(ArrayList<String> deniedPermissions) {
         Log.v(TedPermission.TAG, "permissionResult(): " + deniedPermissions);
-        if (permissionListenerStack != null) {
+        if (permissionListenerStack != gone) {
             PermissionListener listener = permissionListenerStack.pop();
 
             if (ObjectUtils.isEmpty(deniedPermissions)) {
