7a1a92c94df1e480d3d5e58e08d93af2ccd74c61
lutsenko-yuriy <lutsenko.yuriy93@gmail.com>
2018-06-30 20:51:50 +0300

    Slightly refactored

--- tedpermission-kotlin-dsl/src/main/java/com/gun0912/tedpermission_kotlin_dsl/dsl/CheckPermissions.kt
+++ tedpermission-kotlin-dsl/src/main/java/com/gun0912/tedpermission_kotlin_dsl/dsl/CheckPermissions.kt
@@ -5,18 +5,18 @@ package com.gun0912.tedpermission_kotlin_dsl.dsl
 import android.content.Context
 
 inline fun Context.checkPermissions(body: Dsl.() -> Unit) =
-    Dsl(this).apply(body).check()
+        Dsl(this).apply(body).check()
 
 // There are three variants of permission calling
 // because using varargs can cause performance issues
 // and users rarely ask for more than 2 permissions at once
 inline fun Context.checkPermissions(permission: String, body: Dsl.() -> Unit) =
-    Dsl(this)
-            .apply {
-                +permission
-                body()
-            }
-            .check()
+        Dsl(this)
+                .apply {
+                    +permission
+                    body()
+                }
+                .check()
 
 inline fun Context.checkPermissions(firstPermission: String, secondPermission: String, body: Dsl.() -> Unit) =
         Dsl(this)
