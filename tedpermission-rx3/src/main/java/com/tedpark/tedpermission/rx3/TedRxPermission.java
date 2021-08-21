4ad8aef221cf7e308e50d1838657544e228d0434
ted <tkdrnjs0912@gmail.com>
2021-08-21 20:16:45 +0900

    Add rxJava3

---
+++ tedpermission-rx3/src/main/java/com/tedpark/tedpermission/rx3/TedRxPermission.java
@@ -0,0 +1,58 @@
+package com.tedpark.tedpermission.rx3;
+
+import android.content.Context;
+
+import com.gun0912.tedpermission.PermissionBuilder;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermissionBase;
+import com.gun0912.tedpermission.TedPermissionResult;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.SingleEmitter;
+import io.reactivex.rxjava3.core.SingleOnSubscribe;
+
+
+public class TedRxPermission extends TedPermissionBase {
+
+    public static Builder with(Context context) {
+        return new Builder(context);
+    }
+
+    public static class Builder extends PermissionBuilder<Builder> {
+
+        private Builder(Context context) {
+            super(context);
+        }
+
+        public Single<TedPermissionResult> request() {
+            return Single.create(new SingleOnSubscribe<TedPermissionResult>() {
+                @Override
+                public void subscribe(@NonNull final SingleEmitter<TedPermissionResult> emitter) {
+                    PermissionListener listener = new PermissionListener() {
+                        @Override
+                        public void onPermissionGranted() {
+                            emitter.onSuccess(new TedPermissionResult(null));
+                        }
+
+                        @Override
+                        public void onPermissionDenied(List<String> deniedPermissions) {
+                            emitter.onSuccess(new TedPermissionResult(deniedPermissions));
+                        }
+                    };
+
+                    try {
+                        setPermissionListener(listener);
+                        checkPermissions();
+                    } catch (Exception exception) {
+                        emitter.onError(exception);
+                    }
+                }
+            });
+        }
+    }
+
+
+}
