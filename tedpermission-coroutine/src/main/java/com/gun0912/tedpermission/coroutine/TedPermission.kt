72f80b097fde637cff50c50a2c682786abf5df09
ted <tkdrnjs0912@gmail.com>
2021-08-22 14:49:58 +0900

    Add coroutine library

---
+++ tedpermission-coroutine/src/main/java/com/gun0912/tedpermission/coroutine/TedPermission.kt
@@ -0,0 +1,39 @@
+package com.gun0912.tedpermission.coroutine
+
+import android.annotation.SuppressLint
+import com.gun0912.tedpermission.PermissionBuilder
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermissionBase
+import com.gun0912.tedpermission.TedPermissionResult
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+@SuppressLint("StaticFieldLeak")
+object TedPermission : TedPermissionBase() {
+
+    fun create(): Builder = Builder()
+
+
+    class Builder : PermissionBuilder<Builder>() {
+
+        override fun setPermissionListener(listener: PermissionListener): Builder {
+            throw UnsupportedOperationException("Use check() function")
+        }
+
+        suspend fun check(): TedPermissionResult = suspendCoroutine {
+            super.setPermissionListener(object : PermissionListener {
+                override fun onPermissionGranted() {
+                    it.resume(TedPermissionResult(null))
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    it.resume(TedPermissionResult(deniedPermissions))
+                }
+            })
+            checkPermissions()
+        }
+
+        suspend fun checkGranted(): Boolean = check().isGranted
+    }
+
+}
